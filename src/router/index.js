9e7f0f04cabfb9eba5249dc8897995120191aef0
VAIL <131298@FOSSIL.COM>
2019-05-22 14:53:34 -0500

    remove about route

--- src/router/index.js
+++ src/router/index.js
@@ -31,14 +31,6 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/About.vue')
-    },
-    {
       path: '/login',
       name: 'login',
       component: () => import('../views/Login.vue'),
